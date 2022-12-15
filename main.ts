b88a42a7b5da24f5422f39e4f86e2b771d44d89a
417792 <115142684+417792@users.noreply.github.com>
2022-12-15 15:45:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function 燈數 (num: number) {
     if (num % 5 == 0) {
-        x = 0
-        y = Math.floor(num / 0) - 1
+        x = 4
+        y = Math.floor(num / 5) - 1
     } else {
         x = num % 5 - 1
         y = Math.floor(num / 5)
@@ -9,13 +9,33 @@ function 燈數 (num: number) {
     led.plot(x, y)
 }
 input.onButtonPressed(Button.A, function () {
+    b += 1
+    if (a > 0) {
+        a = 25
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    b += -1
+    if (a < 0) {
+        a = 0
+    }
+})
+let n = 0
+let list: number[] = []
+let a = 0
+let b = 0
+let y = 0
+let x = 0
+let _0 = 0
+let _1 = 1
+basic.forever(function () {
     basic.clearScreen()
     list = []
     for (let index = 0; index < 25; index++) {
         list.push(_0)
     }
     n = 0
-    while (n < 3) {
+    while (n < b) {
         a = randint(0, 24)
         if (list[a] == 0) {
             list[a] = _1
@@ -23,16 +43,5 @@ input.onButtonPressed(Button.A, function () {
             燈數(a + 1)
         }
     }
-})
-let a = 0
-let n = 0
-let list: number[] = []
-let y = 0
-let x = 0
-let _1 = 0
-let _0 = 0
-_0 = 0
-_1 = 1
-basic.forever(function () {
-	
+    basic.pause(500)
 })
